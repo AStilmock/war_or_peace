b190e65f7392c866a5a20a75fc6f5b7f544bc2a3
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-03-23 12:34:22 -0600

    Add code for Turn files

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,18 +9,28 @@ class Turn
   def type
     if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
       :basic
-    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
-      :war
     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
     end
   end
 
   def winner
-    if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
-      player1
-    else
-      player2
+    if type == :basic
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        player1
+      else
+        player2
+      end
+    elsif type == :war
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        player1
+      else
+        player2
+      end
+    elsif type == :mutually_assured_destruction
+      return "No winner"
     end
   end
 
@@ -28,9 +38,20 @@ class Turn
     if type == :basic
       @spoils_of_war << @player1.deck.remove_card
       @spoils_of_war << @player2.deck.remove_card
-    # elsif type == :war
-    #   3.times(@spoils_of_war << @player1.deck.remove_card)
-    #   3.times(@spoils_of_war << @player2.deck.remove_card)
+    elsif type == :war
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
+    elsif type == :mutually_assured_destruction
+      @player1.deck.remove_card
+      @player1.deck.remove_card
+      @player1.deck.remove_card
+      @player2.deck.remove_card
+      @player2.deck.remove_card
+      @player2.deck.remove_card
     end
   end
 
